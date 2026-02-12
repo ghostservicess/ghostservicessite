70eedee1cf4ba0651bd14c77b8441efb1c22c15f
ghostservicess <iilkeryasiin1@gmail.com>
2026-02-12 20:18:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,34 +6,34 @@ const cartModal = document.getElementById("cartModal");
 const cartItems = document.getElementById("cartItems");
 const cartTotal = document.getElementById("cartTotal");
 
-function addToCart(name, price){
-    cart.push({name, price});
-    total += price;
-    updateCart();
+function addToCart(name, price) {
+  cart.push({ name, price });
+  total += price;
+  updateCart();
 }
 
-function updateCart(){
-    cartItems.innerHTML = "";
-    cart.forEach(item=>{
-        let li = document.createElement("li");
-        li.textContent = item.name + " - " + item.price + " TL";
-        cartItems.appendChild(li);
-    });
-    cartTotal.textContent = "Toplam: " + total + " TL";
-    cartBtn.textContent = "Sepet (" + cart.length + ")";
+function updateCart() {
+  cartItems.innerHTML = "";
+  cart.forEach(item => {
+    let li = document.createElement("li");
+    li.textContent = item.name + " - " + item.price + " TL";
+    cartItems.appendChild(li);
+  });
+  cartTotal.textContent = "Toplam: " + total + " TL";
+  cartBtn.textContent = "Sepet (" + cart.length + ")";
 }
 
-cartBtn.addEventListener("click", ()=>{
-    cartModal.style.display = "flex";
+cartBtn.addEventListener("click", () => {
+  cartModal.style.display = "flex";
 });
 
-function closeCart(){
-    cartModal.style.display = "none";
+function closeCart() {
+  cartModal.style.display = "none";
 }
 
-/* CURSOR EFFECT */
-document.addEventListener("mousemove", e=>{
-    const glow = document.querySelector(".cursor-glow");
-    glow.style.left = e.clientX + "px";
-    glow.style.top = e.clientY + "px";
+/* Cursor Glow */
+document.addEventListener("mousemove", (e) => {
+  const glow = document.querySelector(".cursor-glow");
+  glow.style.left = e.clientX + "px";
+  glow.style.top = e.clientY + "px";
 });
